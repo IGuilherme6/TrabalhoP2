4cd0fc26c3363c8186f2c1af8eeef864cc49a7c7
Guilherme <guilhermecostagodoi@gmai.com>
2024-05-08 15:40:44 -0300

    Criando Entidades

---
+++ TrabalhoP2/src/main/java/br/unigran/Entrada.java
@@ -0,0 +1,22 @@
+package br.unigran;
+
+import java.util.Date;
+
+public class Entrada {
+
+    private Date dataEntrada;
+    private NotaFiscal notaFiscal_Associada;
+    private String sessao;
+
+    public void Login() {
+
+    }
+
+    public void EfetuarSaida() {
+
+    }
+
+    public void AlterarSaida() {
+
+    }
+}
---
+++ TrabalhoP2/src/main/java/br/unigran/NotaFiscal.java
@@ -0,0 +1,5 @@
+package br.unigran;
+
+public class NotaFiscal {
+
+}
