1f79dcdc0ad49bfa17814735768e69489c432948
Guilherme <guilhermecostagodoi@gmai.com>
2024-05-08 15:49:57 -0300

    Criando classes

---
+++ TrabalhoP2/src/main/java/br/unigran/Cod_Barra.java
@@ -0,0 +1,7 @@
+package br.unigran;
+
+public class Cod_Barra {
+    private String id;
+    private Tipo tipo;
+    private Estoque estoque;
+}
---
+++ TrabalhoP2/src/main/java/br/unigran/Tipo.java
@@ -0,0 +1,8 @@
+package br.unigran;
+
+import java.io.Serial;
+
+public class Tipo {
+    private Integer id;
+    private String descricao;
+}
