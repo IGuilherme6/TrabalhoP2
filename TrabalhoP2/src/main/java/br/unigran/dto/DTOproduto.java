80d26837535bfb3ddd164796e65c29206d1c7d0d
IGuilherme6 <guilhermecostagodoi@gmail.com>
2024-05-28 20:51:04 -0300

    mexendo nos controllers e no DTO

--- TrabalhoP2/src/main/java/br/unigran/dto/DTOproduto.java
+++ TrabalhoP2/src/main/java/br/unigran/dto/DTOproduto.java
@@ -1,6 +1,21 @@
 package br.unigran.dto;
 
+import javax.persistence.Column;
+import java.io.Serial;
+import java.util.Date;
+
 public class DTOproduto extends DTO{
+    private String id;
+    private String nome;
+    private Date dataValidade;
+    private String marca;
+    private String categoria;
+    private String fornecedor;
+    private Double precoProduto;
+    private Double precoVenda;
+    private Integer quantRecebida;
+    private Integer quant_min_estoque;
+
     @Override
     public Object builder() {
         return null;
