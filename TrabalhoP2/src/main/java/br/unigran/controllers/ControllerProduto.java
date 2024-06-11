7aa109c072f0bbdc81d3115956fb9eeb0a40d2fe
michelmanzatotebar <zsouta22@gmail.com>
2024-06-10 22:09:32 -0400

    cadastro produtos

--- TrabalhoP2/src/main/java/br/unigran/controllers/ControllerProduto.java
+++ TrabalhoP2/src/main/java/br/unigran/controllers/ControllerProduto.java
@@ -1,11 +1,15 @@
 package br.unigran.controllers;
 
+import br.unigran.banco.ProdutosDao;
+import br.unigran.entidades.Produtos;
+
 import java.util.List;
 
 
 public class ControllerProduto {
-    public void CadastrarProduto(){
-
+    public void CadastrarProduto(Produtos produtos){
+        ProdutosDao produtosDao = new ProdutosDao();
+        produtosDao.Salvar(produtos);
     }
 
     public void AlterarProduto(){
